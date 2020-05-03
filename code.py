679776aa78b0f7a6e07e0cca865a13c09c66214d
joanss2 <64496738+joanss2@users.noreply.github.com>
2020-05-03 20:08:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -16,3 +16,24 @@ with open('sequences.csv','r') as seq_file:
         trobat = False
 
     print(list)
+'''
+import csv
+
+with open('sequences_table.csv','r') as seq_file:
+    csv_reader = csv.DictReader(seq_file)
+
+    list = []
+    trobat = False
+
+    for seq in csv_reader:
+        for j in range(len(list)):
+            if seq['Geo_Location'] == list[j][0]:
+                trobat = True
+                list[j].append(seq['Length'])
+                j = len(list)-1
+        if trobat == False:
+            list.append([seq['Geo_Location'],seq['Length']])
+        trobat = False
+
+    print(list)
+'''
