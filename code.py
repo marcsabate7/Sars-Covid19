ab20ba0dacd50b48fce96a373386798239f903a2
marcsabate7 <64595179+marcsabate7@users.noreply.github.com>
2020-05-06 18:53:50 +0200

    Update code.py
    Calculem la mediana de cada pais, i la fiquem en una llista junt amb el nom del pais. D'aquesta manera tenim la llista i el nom i podrem trobar el seu accesion per accedir al ARN.

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def completeList():
     with open('sequences.csv','r') as seq_file:
         csv_reader = csv.DictReader(seq_file)
@@ -12,34 +11,38 @@ def completeList():
             for j in range(len(list)):
                 if seq['Geo_Location'] == list[j][0]:
                     trobat = True
-                    list[j].append(seq['Length'])
+                    list[j][1].append(int(seq['Length']))
                     j = len(list)-1
             if trobat == False:
-                list.append([seq['Geo_Location'],seq['Length']])
+                list.append([seq['Geo_Location'],[int(seq['Length'])]])
             trobat = False
     return list
 
-    
-def ordenarArray(arr):
-    for index in range(1, len(arr)):
-        current = arr[index]
-        position = index
-
-        while position > 0 and arr[position-1] > current:
-            arr[position] = arr[position-1]
-            position -= 1
-
-        arr[position] = current
-
-    print(arr)
-    
-    
-    
 def modify(list):
     for i in range(len(list)):
-        ordenarArray(list[i])
+        list[i][1].sort()
+    
+    return list
 
+def calculMediana(arr):
+    list = []
+    for i in range(len(arr)-1):
+        long = len(arr[i][1])
+        if long % 2 != 0:
+            total = int(round((long/2)))
+            num = arr[i][1]
+            numbo = num[total-1]
+        else:
+            total = int((long/2)+1)
+            num = arr[i][1]
+            numbo = num[total-1]
+        total = 0
+        list.append([arr[i][0],numbo])
+    print(list)
+    
     
 if __name__=="__main__":
     list = completeList()
-    modify(list)
+    arr = modify(list)
+    calculMediana(arr)
+
