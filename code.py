62f5054dcf711ff834f0f2f85f249749c654e6ba
marcsabate7 <64595179+marcsabate7@users.noreply.github.com>
2020-05-06 15:03:02 +0200

    Update code.py
    Added functions and insertion method

--- code.py
+++ code.py
@@ -1,18 +1,45 @@
 import csv
 
-with open('sequences.csv','r') as seq_file:
-    csv_reader = csv.DictReader(seq_file)
 
-    list = []
-    trobat = False
+def completeList():
+    with open('sequences.csv','r') as seq_file:
+        csv_reader = csv.DictReader(seq_file)
 
-    for seq in csv_reader:
-        for j in range(len(list)):
-            if seq['Geo_Location'] == list[j]:
-                trobat = True
-                j = len(list)-1
-        if trobat == False:
-            list.append(seq['Geo_Location'])
+        list = []
         trobat = False
 
-    print(list)
+        for seq in csv_reader:
+            for j in range(len(list)):
+                if seq['Geo_Location'] == list[j][0]:
+                    trobat = True
+                    list[j].append(seq['Length'])
+                    j = len(list)-1
+            if trobat == False:
+                list.append([seq['Geo_Location'],seq['Length']])
+            trobat = False
+    return list
+
+    
+def ordenarArray(arr):
+    for index in range(1, len(arr)):
+        current = arr[index]
+        position = index
+
+        while position > 0 and arr[position-1] > current:
+            arr[position] = arr[position-1]
+            position -= 1
+
+        arr[position] = current
+
+    print(arr)
+    
+    
+    
+def modify(list):
+    for i in range(len(list)):
+        ordenarArray(list[i])
+
+    
+if __name__=="__main__":
+    list = completeList()
+    modify(list)
